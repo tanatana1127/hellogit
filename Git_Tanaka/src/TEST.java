169946211243ab5e0a8ebc3f24f2794cb1224ae4
sailing_2 <sailing_2@192.168.100.8>
2016-07-29 14:54:15 +0900

    回数だすよ

--- Git_Tanaka/src/TEST.java
+++ Git_Tanaka/src/TEST.java
@@ -4,6 +4,7 @@ public class TEST {
 	public static void main(String[] args) {
 		int x=0;
 
+		System.out.println("5‰ñ‚­‚è‚©‚¦‚·‚æ");
 		while(true){
 			System.out.println("Gitg‚¤‚æII");
 			x++;
